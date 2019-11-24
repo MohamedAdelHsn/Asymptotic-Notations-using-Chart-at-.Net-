6ceb401bc29b025d316b18076b5fe802fe2f4242
MohamedAdelHsn <58120325+MohamedAdelHsn@users.noreply.github.com>
2019-11-24 02:09:56 +0200

    Add files via upload

---
+++ TestComplexity OfAlgorithm/TestComplexity OfAlgorithm/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace TestComplexity_OfAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 32);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(872, 371);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(415, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 508);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TestComplexity OfAlgorithm/TestComplexity OfAlgorithm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TestComplexity_OfAlgorithm
+{
+   
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private int[] arr = new int[] { 25000, 50000, 75000, 100000, 250000, 500000, 1000000 };
+             
+
+        private double test_complexity(int size)
+        {
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            st.Stop();
+
+            return Convert.ToDouble(st.ElapsedMilliseconds);
+
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            chart1.Series.Clear();
+            chart1.Series.Add(new Series("Time complexity of loop : O(n)"));
+            chart1.Series[0].Points.AddXY(arr[0], test_complexity(arr[0]));
+            chart1.Series[0].Points.AddXY(arr[1], test_complexity(arr[1]));
+            chart1.Series[0].Points.AddXY(arr[2], test_complexity(arr[2]));
+            chart1.Series[0].Points.AddXY(arr[3], test_complexity(arr[3]));
+            chart1.Series[0].Points.AddXY(arr[4], test_complexity(arr[4]));
+            chart1.Series[0].Points.AddXY(arr[5], test_complexity(arr[5]));
+            chart1.Series[0].Points.AddXY(arr[6], test_complexity(arr[6]));
+
+
+            chart1.Series[0].Color = Color.Black;
+            chart1.Series[0].BorderWidth = 2;
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+
+            
+
+        }
+
+ 
+        
+
+        }
+    }
+
+
